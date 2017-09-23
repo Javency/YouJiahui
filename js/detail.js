bad739c4674889fcf543c705b6bd6d02249d0983
zhangwenjia <839254349@qq.com>
2017-09-23 17:06:35 +0800

    优家会第一次提交

---
+++ js/detail.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".goods_dy").on("click",function(){
+		$(".goods_dy").children("a").attr("href",'详情页-details.html');
+	});
+	$(".boxCon_spDl").find("a").on("click",function(){
+		$(".boxCon_spDl").find("a").attr("href",'详情页-details.html');
+	});
+});
